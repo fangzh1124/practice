716e52cb54355e9fb520475c0b0b0f4421ad3321
Fang Zhang <fzhang@thoughtworks.com>
2015-04-01 23:07:14 +0800

    Clean version. Removed over designed part. converted two lengths to cm directly.

--- ooBootCamp/warmup/src/LengthComparator.js
+++ ooBootCamp/warmup/src/LengthComparator.js
@@ -1,43 +1,25 @@
 function CompareLength(){
-	var parameters = Array.prototype.slice.call(arguments);
-
 	this.isSameLength = function(length1, length2){
-		var lengthDetail1 = lengthParser(length1),
-			  lengthDetail2 = lengthParser(length2),
-				result = true;
+		var num1 = convertLengthToCM(length1),
+			  num2 = convertLengthToCM(length2);
 
-		if(lengthDetail1[0] != lengthDetail2[0] || lengthDetail1[1] != lengthDetail2[1]){
-			lengthDetail1 = convertLength(lengthDetail1);
-			lengthDetail2 = convertLength(lengthDetail2);
-			if(lengthDetail1[0] != lengthDetail2[0]){
-				result = false;
-			}
+		if(num1 == num2){
+			return true;
 		}
-
-		return result;
+		return false;
 	}
 
-	function lengthParser(length){
-		var digit, unit;
-		if(length.indexOf('cm') > -1 || length.indexOf('dm') > -1){
-			digit = length.slice(0,length.length-2);
-			unit = length.slice(length.length-2);
+	function convertLengthToCM(length){
+		var num;
+
+		if(length.indexOf('cm') > -1){
+			num = parseFloat(length.slice(0,length.length-2));
+		}else if(length.indexOf('dm') > -1){
+			num = parseFloat(length.slice(0,length.length-2)) * 10;
 		}else{
-			digit = length.slice(0,length.length-1);
-			unit = length.slice(length.length-1);
+			num = parseFloat(length.slice(0,length.length-1)) * 100;
 		}
-		digit = parseFloat(digit);
-
-		return [digit,unit];
-	}
-
-	function convertLength(lengthDetail){
-		if(lengthDetail[1] == 'dm'){
-			lengthDetail[0] *= 10; 
-		}else if(lengthDetail[1] == 'm'){
-			lengthDetail[0] *= 100; 
-		}
-		return lengthDetail;
+		return num;
 	}
 }
 
