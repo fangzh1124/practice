4e00a9e76795ba474073f78579166d729d89258b
Fang Zhang <fzhang@thoughtworks.com>
2015-04-14 18:41:20 +0800

    added smart boy

--- ooBootCamp/parkinglot/Parkinglot.js
+++ ooBootCamp/parkinglot/Parkinglot.js
@@ -8,7 +8,7 @@ describe("ParkingLot", function() {
 	var ticket = new Ticket();
 	var parkingBoy = new ParkingBoy();
 
-	
+	// Parking Lot Tests
 	it('should pick the same car when stored a car', function(){
 		parkingLot.pickCar(parkingLot.storeCar(car1)).should.equals(car1);
 	});
@@ -22,23 +22,79 @@ describe("ParkingLot", function() {
 		parkingLot.pickCar(ticket).should.equals('');
 	});
 
-	//ParkingBoy
+	// Parking Boy Tests
 	it('should pick the same car when stored a car by a parkingBoy', function(){
-		var parkingBoy = new ParkingBoy();
-		var parkingLot1 = new ParkingLot(1);
-		var parkingLot2 = new ParkingLot(1);
-		parkingBoy.addParkingLot(parkingLot1);
-		parkingBoy.addParkingLot(parkingLot2);
+		var parkingBoy = new ParkingBoy([new ParkingLot(1)]);
 		var car = new Car();
 
 		parkingBoy.pickCar(parkingBoy.storeCar(car)).should.equals(car);
-
 	});
+
+	// Smart Parking Boy Tests
+	it('should pick the same car when stored a car by a smart parking Boy', function(){
+		var smartParkingBoy = new SmartParkingBoy([new ParkingLot(1)]);
+		var car = new Car();
+
+		smartParkingBoy.pickCar(smartParkingBoy.storeCar(car)).should.equals(car);
+	});	
+
+	it('should park in a more avaible parkinglot Given two parkinglots with different size When store a car by smart Parking boy', function(){
+		var smartParkingBoy = new SmartParkingBoy([new ParkingLot(1), new ParkingLot(2)]);
+		smartParkingBoy.storeCar(new Car());
+
+		smartParkingBoy.parkingLots[1].availableSpace().should.equals(1);
+	});	
+
+	it('should not store a car Given a full parkinglot When store a car by smart Parking boy', function(){
+		var smartParkingBoy = new SmartParkingBoy([new ParkingLot(1)]);
+		smartParkingBoy.storeCar(new Car());
+
+		smartParkingBoy.storeCar(new Car()).should.equals('');
+	});			
 });
 
+
 // Solution Part
-function ParkingBoy(){
-	this.parkingLots = [];
+
+// Smart Parking Boy
+function SmartParkingBoy(parkinglots){
+	this.parkingLots = parkinglots;
+
+	this.getMostAvailableParkingLot = function(){
+		var mostAvailableParkingLot = '';
+		var freeSpace = 0;
+		for (var i = 0; i < this.parkingLots.length; i++) {
+			if(this.parkingLots[i].availableSpace() > freeSpace){
+				mostAvailableParkingLot = this.parkingLots[i];
+				freeSpace = this.parkingLots[i].availableSpace();
+			}
+		}
+		return mostAvailableParkingLot;
+	}
+
+	this.storeCar = function(car){
+		var ticket = '';
+		var mostAvailableParkingLot = this.getMostAvailableParkingLot();
+		if(mostAvailableParkingLot){
+			ticket = mostAvailableParkingLot.storeCar(car);
+		}
+		return ticket;
+	}
+
+	this.pickCar = function(ticket){
+		var car = '';
+		for (var i = 0; i < this.parkingLots.length; i++) {
+			if(car = this.parkingLots[i].pickCar(ticket)){
+				break;
+			}
+		}
+		return car;
+	}
+}
+
+// Parking Boy
+function ParkingBoy(parkinglots){
+	this.parkingLots = parkinglots;
 
 	this.addParkingLot = function(parkingLot){
 		this.parkingLots.push(parkingLot);
@@ -64,6 +120,7 @@ function ParkingBoy(){
 	}
 }
 
+// Parking Lot
 function ParkingLot(size){
 	this.size = size;
 	this.cars = [];
@@ -87,6 +144,10 @@ function ParkingLot(size){
 	    return this.cars.splice(index, 1)[0][1];
 	  }
 	  return '';
+	}
+
+	this.availableSpace = function(){
+		return this.size - this.cars.length;
 	}
 };
 
